90d938e89740c40d600e1cf797f0d0007553d385
bLuDd1 <dtavluj@gmail.com>
2022-12-17 00:39:34 +0200

    Initial Commit

---
+++ Reservations/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Reservations
+//
+//  Created by Dima Tavlui on 17.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Dima Tavlui on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
