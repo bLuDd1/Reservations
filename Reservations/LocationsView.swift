1410315253e455a3b866a8371a20714b91f8927f
bLuDd1 <dtavluj@gmail.com>
2023-01-14 20:44:33 +0200

    Added LocationsView file

---
+++ Reservations/LocationsView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct LocationsView: View {
+    @EnvironmentObject var model: Model
+    var body: some View {
+        VStack {
+            LittleLemonLogo()
+                .padding(.top, 50)
+            Text(model.displayingReservationForm ? "Reservation Details" :
+                    "Select a location")
+            .padding([.leading, .trailing], 40)
+            .padding([.top, .bottom], 8)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(20)
+            
+            NavigationView {
+                List(model.restaurants, id: \.self) { restaurant in
+                    NavigationLink(destination: ReservationForm(restaurant)) {
+                        RestaurantView(restaurant)
+                    }
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                }
+            }
+            .onDisappear {
+                if model.tabBarChanged { return }
+                model.displayingReservationForm = true
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.top, -10)
+            .scrollContentBackground(.hidden)
+        }
+    }
+}
+
+struct LocationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsView().environmentObject(Model())
+    }
+}
