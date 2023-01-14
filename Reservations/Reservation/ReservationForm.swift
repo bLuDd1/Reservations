2ec276f0b16efba0efd9258d87eeb54ebac2346b
bLuDd1 <dtavluj@gmail.com>
2023-01-14 20:34:44 +0200

    Added validation functions

--- Reservations/Reservation/ReservationForm.swift
+++ Reservations/Reservation/ReservationForm.swift
@@ -64,22 +64,22 @@ struct ReservationForm: View {
                             TextField("Your name...", text: $customerName)
                         }
                         HStack {
-                            Text("PHONE: ")
+                            Text("Phone: ")
                                 .font(.subheadline)
                             
                             TextField("Your phone number...", text: $customerPhoneNumber)
-                            .textContentType(.telephoneNumber)
-                            .keyboardType(.phonePad)
+                                .textContentType(.telephoneNumber)
+                                .keyboardType(.phonePad)
                         }
                         HStack {
-                            Text("E-MAIL: ")
+                            Text("Email: ")
                                 .font(.subheadline)
                             
-                            TextField("Your e-mail...", text: $customerEmail)
-                            .keyboardType(.emailAddress)
-                            .textContentType(.emailAddress)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
+                            TextField("Your email...", text: $customerEmail)
+                                .keyboardType(.emailAddress)
+                                .textContentType(.emailAddress)
+                                .disableAutocorrection(true)
+                                .autocapitalization(.none)
                         }
                         TextField("add any special request (optional)",
                                   text: $specialRequests,
@@ -88,6 +88,17 @@ struct ReservationForm: View {
                         .overlay( RoundedRectangle(cornerRadius: 20).stroke(.gray.opacity(0.2)) )
                         .lineLimit(6)
                         .padding([.top, .bottom], 20)
+                    }
+                    Button(action: {
+                        validateForm()
+                    }, label: {
+                        Text("Confirm reservation")
+                    })
+                    .padding(.init(top: 10, leading: 30, bottom: 10, trailing: 30))
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(20)
+                    .padding(.top, 10)
                     }
                 }
                 .padding(.top, -40)
@@ -108,11 +119,70 @@ struct ReservationForm: View {
                 model.displayingReservationForm = false
             }
         }
+        
+        private func validateForm() {
+            let nameIsValid = isValid(name: customerName)
+            let emailIsValid = isValid(email: customerEmail)
+            
+            guard nameIsValid && emailIsValid
+            else {
+                var invalidNameMessage = ""
+                if customerName.isEmpty || !isValid(name: customerName) {
+                    invalidNameMessage = "Names can only contain letters and must have at least 3 characters\n\n"
+                }
+                
+                var invalidPhoneMessage = ""
+                if customerEmail.isEmpty {
+                    invalidPhoneMessage = "The phone number cannot be blank.\n\n"
+                }
+                
+                var invalidEmailMessage = ""
+                if !customerEmail.isEmpty || !isValid(email: customerEmail) {
+                    invalidEmailMessage = "The e-mail is invalid and cannot be blank."
+                }
+                
+                self.errorMessage = "Found these errors in the form:\n\n \(invalidNameMessage)\(invalidPhoneMessage)\(invalidEmailMessage)"
+                
+                showFormInvalidMessage.toggle()
+                return
+            }
+            
+            let temporaryReservation = Reservation(restaurant: restaurant,
+                                                   customerName: customerName,
+                                                   customerEmail: customerEmail,
+                                                   customerPhoneNumber: customerPhoneNumber,
+                                                   reservationDate: reservationDate,
+                                                   party: party,
+                                                   specialRequests: specialRequests)
+            
+            self.temporaryReservation = temporaryReservation
+            self.mustChangeReservation.toggle()
+            self.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func isValid(name: String) -> Bool {
+            guard !name.isEmpty,
+                  name.count > 2
+            else { return false }
+            for chr in name {
+                if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") && !(chr == " ") ) {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        func isValid(email:String) -> Bool {
+            guard !email.isEmpty else { return false }
+            let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+            let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
+            return emailValidationPredicate.evaluate(with: email)
+        }
     }
-}
 
 struct ReservationForm_Previews: PreviewProvider {
     static var previews: some View {
-        ReservationForm()
+        let sampleRestaurant = RestaurantLocation(city: "Las Vegas", neighborhood: "Downtown", phoneNumber: "(702) 555-9898")
+        ReservationForm(sampleRestaurant).environmentObject(Model())
     }
 }
